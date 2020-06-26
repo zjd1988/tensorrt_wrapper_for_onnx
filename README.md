d999a31729db34cca082d392caa5d7d456b61233
zjd1988 <942002795@qq.com>
2020-06-26 17:43:26 +0800

    add yolov3 tiny/spp example

--- README.md
+++ README.md
@@ -14,15 +14,19 @@ coding ...
 # verified models
 * nvidia-gpu  GTX1060(3GB)
 
-1. lenet  ----------- ./example/lenet/lenet_simplify.onnx ---------------------- verified(fp16/fp32)
+1. lenet  ------------- ./example/lenet/lenet_simplify.onnx ---------------------- verified(fp16/fp32)
 
-2. mobilenet_v2 ----- ./example/mobilenet_v2_simplify.onnx --------------------- verified(fp32)
+2. mobilenet_v2 ------- ./example/mobilenet_v2_simplify.onnx --------------------- verified(fp32)
 
-3. vgg -------------- ./example/vgg/vgg_simplify.onnx -------------------------- verified(fp32)
+3. vgg ---------------- ./example/vgg/vgg_simplify.onnx -------------------------- verified(fp32)
 
-4. squeezenet ------- ./example/squeezenet/squeeze_simplify.onnx --------------- verified(fp32)
+4. squeezenet --------- ./example/squeezenet/squeeze_simplify.onnx --------------- verified(fp32)
 
-5. yolov3 ---------- ./example/yolov3/yolov3_simplify.onnx --------------------- verified(fp32)
+5. yolov3 ------------- ./example/yolov3/yolov3_simplify.onnx --------------------- verified(fp32)
+
+6. yolov3-tiny -------- ./example/yolov3/yolov3-tiny_simplify.onnx ---------------- verified(fp32)
+
+7. yolov3-spp --------- ./example/yolov3/yolov3-spp_simplify.onnx ----------------- verified(fp32)
 
 # step 1
 1. cd tesensorrt_wrapper_for_onnx
---
+++ example/yolov3-spp_example.cpp
@@ -0,0 +1,86 @@
+#include "json/json.h"
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <chrono>
+#include "tensorrt_engine.hpp"
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace tensorrtInference;
+
+#define NET_NAME "./example/yolov3-spp/"
+#define GRAPH_JSON_FILE(net)    net "net_graph.json"
+#define GRAPH_WEIGHTS_FILE(net) net "net_weights.bin"
+#define GRAPH_ENGINE_FILE(net)  net "net.engine"
+#define SAVE_ENGINE 0
+#define FP16_FLAG false
+
+#define BACTCH_SIZE 1
+#define CHANNEL_SIZE 3
+#define HEIGHT_SIZE 320
+#define WIDTH_SIZE 192
+
+#define REGION_SIZE 3780
+#define CLASSES_SIZE 80
+#define BOXES_SIZE 4
+
+void initInputData(std::map<std::string, void*> hostMemMap)
+{
+    int size = BACTCH_SIZE * CHANNEL_SIZE * HEIGHT_SIZE * WIDTH_SIZE;
+    auto input = (float*)(hostMemMap["images"]);
+    for(int i = 0; i < size; i++)
+    {
+        input[i] = 1.0f;
+    }
+}
+void printOutputData(std::map<std::string, void*> hostMemMap)
+{
+    auto classes_output = (float*)(hostMemMap["classes"]);
+    int start = 3 * CLASSES_SIZE;
+    int end = start + 10;
+    for(int i = start; i < end; i++)
+    {
+        std::cout << classes_output[i] << std::endl;
+    }
+    auto boxes_output = (float*)(hostMemMap["boxes"]);
+    for(int i = 0; i < 10; i++)
+    {
+        std::cout << boxes_output[i] << std::endl;
+    }    
+
+}
+
+int main()
+{
+    std::string jsonFileName    = GRAPH_JSON_FILE(NET_NAME);
+    std::string weightsFileName = GRAPH_WEIGHTS_FILE(NET_NAME);
+    std::string engineFileName  = GRAPH_ENGINE_FILE(NET_NAME);
+    // save engine file
+#if SAVE_ENGINE
+    tensorrtEngine engine(jsonFileName, weightsFileName, FP16_FLAG);
+    engine.saveEnginePlanFile(engineFileName);
+#else
+    //engine inference
+    // std::string bmpFile = "gray_test.bmp";
+    // cv::Mat colorBmp = cv::imread(bmpFile.c_str());
+    // cv::Mat grayBmp;
+    // cv::Mat inputBmp( 721, 1281, CV_8UC1, cv::Scalar(0));
+    // cv::Rect rect(0, 0, 1280, 720);
+    // cv::cvtColor(colorBmp, grayBmp, cv::COLOR_BGR2GRAY);
+    // grayBmp.copyTo(inputBmp(rect));
+    // cv::Mat inputFloatBmp( 721, 1281, CV_32FC1, cv::Scalar(0));
+    // inputBmp.convertTo(inputFloatBmp, CV_32F);
+    tensorrtEngine engine(engineFileName);
+    auto hostMem = engine.getBindingNamesHostMemMap();
+    initInputData(hostMem);
+    
+    for (int i = 0; i < 100; i++) {
+        auto start = std::chrono::system_clock::now();
+        engine.doInference(true);
+        auto end = std::chrono::system_clock::now();
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+    }
+    printOutputData(hostMem);
+#endif
+}

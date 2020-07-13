0b0b0e17a96ced2a83f6fcab9871bd17b5f05cec
zjd1988 <942002795@qq.com>
2020-07-13 20:33:29 +0800

    update execution.py and test python file

--- python_scripts/execution.py
+++ python_scripts/execution.py
@@ -4,161 +4,313 @@ import json
 import os
 import sys
 import numpy as np
+import cv2
+import copy
 
-onnx_data_type = {}
-onnx_data_type["UNDEFINED"] = 0
-onnx_data_type["FLOAT"] = 1
-onnx_data_type["UINT8"] = 2
-onnx_data_type["INT8"] = 3
-onnx_data_type["UINT16"] = 4
-onnx_data_type["INT16"] = 5
-onnx_data_type["INT32"] = 6
-onnx_data_type["INT64"] = 7
-onnx_data_type["STRING"] = 8
-onnx_data_type["BOOL"] = 9
-onnx_data_type["FLOAT16"] = 10
-onnx_data_type["DOUBLE"] = 11
-onnx_data_type["UINT32"] = 12
-onnx_data_type["UINT64"] = 13
-onnx_data_type["COMPLEX64"] = 14
-onnx_data_type["COMPLEX128"] = 15
-onnx_data_type["BFLOAT16"] = 16
+class Execution:
+    execution_index = 0
+    def __init__(self, execution_name, execution_input_name, execution_output_name):
+        self.name = execution_name + "_" + str(Execution.execution_index)
+        self.type = execution_name
+        self.attr = {}
+        self.input_name = []
+        self.output_name = []
+        Execution.execution_index += 1
+        self.set_inputs(execution_input_name)
+        self.set_outputs(execution_output_name)
+    def get_inputs(self):
+        return self.input_name
+    def get_outputs(self):
+        return self.output_name
+    def set_inputs(self, names):
+        self.input_name = names
+    def set_outputs(self, names):
+        self.output_name = names
+    def get_execution_info(self):
+        info = {}
+        info["type"] = self.type
+        info["attr"] = self.attr
+        info["inputs"] = self.input_name
+        info["outputs"] = self.output_name
+        return info
 
-attr_type = {}
-attr_type["UNDEFINED"] = 0
-attr_type["FLOAT"] = 1
-attr_type["INT"] = 2
-attr_type["STRING"] = 3
-attr_type["TENSOR"] = 4
-attr_type["GRAPH"] = 5
-attr_type["FLOATS"] = 6
-attr_type["INTS"] = 7
-attr_type["STRINGS"] = 8
-attr_type["TENSORS"] = 9
-attr_type["GRAPHS"] = 10
-attr_type["SPARSE_TENSOR"] = 11
-attr_type["SPARSE_TENSORS"] = 12
+class DataTypeConvertExecution(Execution):
+    def __init__(self, execution_input_name, execution_output_name):
+        Execution.__init__(self, "DataTypeConvertExecution", execution_input_name, execution_output_name)
 
-PRE_EXECUTION_RUN_FUNC = {}
-POST_EXECUTION_RUN_FUNC = {}
-EXECUTION_RUN_FUNC = {}
-# execution_type
-# attribute
-# supported type as follows: 
-datatype_convert_set = set()
-datatype_convert_set.add("ConvertUint8ToFloat32")
-datatype_convert_set.add("ConvertUint8ToFloat16")
-def datatype_convert_func(input_data, attr):
+    def init_attr(self, attr):
+        self.attr["convert_type"] = attr["convert_type"]
 
-def create_datatype_convert_execution(convert_type):
-    execution = {}
-    attr = {}
-    if convert_type in datatype_convert_set:
-        attr["convert_type"] = convert_type
-    else:
-        print("not supported type {}".format(convert_type))
-        assert False
-    execution["type"] = "DataTypeConvert"
-    execution["attr"] = attr
-    execution["func"] = datatype_convert_func
+    def execute(self, input_data):
+        result_data = []
+        assert(len(input_data) == 1)
+        if self.attr["convert_type"] == "ConvertUint8ToFloat32":
+            result_data.extend(input_data[0].astype(np.float32))
+        elif self.attr["convert_type"] == "ConvertUint8ToFloat16":
+            result_data.extend(input_data[0].astype(np.float16))
+        else:
+            print("not supported type {}".format(attr["convert_type"]))
+            assert False
+        output_name = self.get_outputs()
+        return dict(zip(output_name,result_data))
 
+class DataFormatConvertExecution(Execution):
+    def __init__(self, execution_input_name, execution_output_name):
+        Execution.__init__(self, "DataFormatConvertExecution", execution_input_name, execution_output_name)
 
-# supported type as follows:
-format_convert_set = set()
-format_convert_set.add("RGB2BGR")
-format_convert_set.add("BGR2RGB")
-format_convert_set.add("RGB2GRAY")
-format_convert_set.add("BGR2GRAY")
-def create_format_convert_execution(convert_type):
-    execution = {}
-    attr = {}
-    if convert_type in format_convert_set:
-        attr["convert_type"] = convert_type
-    else:
-        print("not supported type {}".format(convert_type))
-        assert False
-    execution["type"] = "FormatConvert"
-    execution["attr"] = attr
-    return execution
+    def init_attr(self, attr):
+        self.attr["convert_type"] = attr["convert_type"]
 
+    def execute(self, input_data):
+        result_data = []
+        assert(len(input_data) == 1)
+        if self.attr["convert_type"] == "RGB2BGR":
+            result_data.append(cv2.cvtColor(input_data[0], cv2.COLOR_RGB2BGR))
+        elif self.attr["convert_type"] == "BGR2RGB":
+            result_data.append(cv2.cvtColor(input_data[0], cv2.COLOR_BGR2RGB))
+        elif self.attr["convert_type"] == "RGB2GRAY":
+            result_data.append(cv2.cvtColor(input_data[0], cv2.COLOR_RGB2GRAY))
+        elif self.attr["convert_type"] == "BGR2GRAY":
+            result_data.append(cv2.cvtColor(input_data[0], cv2.COLOR_BGR2GRAY))
+        else:
+            print("not supported type {}".format(attr["convert_type"]))
+            assert False
+        output_name = self.get_outputs()
+        return dict(zip(output_name,result_data))
 
-def create_transpose_execution(axes):
-    execution = {}
-    attr = {}
-    axes_len = len(axes)
-    transpose_axes = []
-    for i in range(axes_len):
-        transpose_axes.append(int(axes[0]))
-    attr["axes"] = transpose_axes
-    execution["type"] = "Transpose"
-    execution["attr"] = attr
-    return execution
+class ReshapeExecution(Execution):
+    def __init__(self, execution_input_name, execution_output_name):
+        Execution.__init__(self, "ReshapeExecution", execution_input_name, execution_output_name)
 
+    def init_attr(self, attr):
+        self.attr["shape"] = attr["shape"]
 
-def create_yolo_nms_execution(img_height, img_width, conf_thresh = 0.3, iou_thresh = 0.6, topk = 200):
-    execution = {}
-    attr = {}
-    attr["img_height"]  = int(img_height)
-    attr["img_width"]   = int(img_width)
-    attr["conf_thresh"] = float(conf_thresh)
-    attr["iou_thresh"]  = float(iou_thresh)
-    attr["topk"]        = int(topk)
-    execution["attr"] = attr
-    return execution
+    def execute(self, input_data):
+        result_data = []
+        assert(len(input_data) == 1)
+        shape = self.attr["shape"]
+        result_data.append(np.reshape(input_data[0], shape))
+        output_name = self.get_outputs()
+        return dict(zip(output_name,result_data))
+
+class TransposeExecution(Execution):
+    def __init__(self, execution_input_name, execution_output_name):
+        Execution.__init__(self, "TransposeExecution", execution_input_name, execution_output_name)
 
 
-def create_normalization_execution(alpha = 0.0, beta = 255.0):
-    execution = {}
-    attr = {}
-    attr["alpha"] = float(alpha)
-    attr["beta"] = float(beta)
-    execution["type"] = "Normalization"
-    execution["attr"] = attr
-    return execution
+    def init_attr(self, attr):
+        self.attr["perm"] = attr["perm"]
+
+    def execute(self, input_data):
+        result_data = []
+        assert(len(input_data) == 1)
+        if "perm" in self.attr.keys():
+            perm = self.attr["perm"]
+            result_data.append(input_data[0].transpose(perm))
+        else:
+            print("have no perm attr!")
+            assert False
+        output_name = self.get_outputs()
+        return dict(zip(output_name,result_data))
+
+class NormalizationExecution(Execution):
+    def __init__(self, execution_input_name, execution_output_name):
+        Execution.__init__(self, "NormalizationExecution", execution_input_name, execution_output_name)
+        self.attr["alpha"] = 0.0
+        self.attr["beta"] = 1.0
 
 
-def construct_onnx_session(onnx_file, pre_execution = [], post_execution = []):
-    onnx_session = {}
-    session = onnxruntime.InferenceSession(onnx_file)
-    inputs = session.get_inputs()
-    inputs_name = []
-    for i in len(inputs):
-        input_name = session.get_inputs()[0].name
-        inputs_name.append(input_name)
+    def init_attr(self, attr):
+        for attr_key, attr_value in attr.items():
+            if attr_key in self.attr:
+                self.attr[attr_key] = attr_value
+            else:
+                pass
+
+    def execute(self, input_data):
+        result_data = []
+        assert(len(input_data) == 1)
+        alpha = self.attr["alpha"]
+        beta = self.attr["beta"]
+        normalize_result = (input_data[0] - alpha) / beta
+        result_data.append(normalize_result.astype(np.float32))
+        output_name = self.get_outputs()
+        return dict(zip(output_name,result_data))
+
+class OnnxExecution(Execution):
+    def __init__(self, onnx_file, execution_input_name, execution_output_name):
+        Execution.__init__(self, "OnnxExecution", execution_input_name, execution_output_name)
+        self.session = onnxruntime.InferenceSession(onnx_file)
+        self.attr["onnx_file"] = onnx_file
+
+    def init_attr(self, attr):
+        pass
+
+    def execute(self, input_data):
+        input_name = self.get_inputs()
+        assert(len(input_data) == len(input_name))
+        output_name = self.get_outputs()
+        results = self.session.run(output_name, dict(zip(input_name,input_data)))
+        return dict(zip(output_name,results))
+
+
+class YoloNMSExecution(Execution):
+    def __init__(self, execution_input_name, execution_output_name):
+        Execution.__init__(self, "YoloNMSExecution", execution_input_name, execution_output_name)
+        self.attr["img_height"]  = 512
+        self.attr["img_width"]   = 384
+        self.attr["conf_thresh"] = 0.3
+        self.attr["iou_thresh"]  = 0.6
+        self.attr["topk"]        = 200
+
+
+    def init_attr(self, attr):
+        for attr_key, attr_value in attr.items():
+            if attr_key in self.attr:
+                self.attr[attr_key] = attr_value
+            else:
+                pass
     
-    if len(pre_execution) == 0:
-        temp_execution = create_datatype_convert_execution("ConvertUint8ToFloat32")
-        pre_execution.append(temp_execution)
+    def xywh2xyxy(self, x, strideX, strideY):
+        # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+        y = np.zeros(x.shape, dtype=x.dtype)
+        y[:, 0] = x[:, 0] - x[:, 2] * strideX / 2  # top left x
+        y[:, 1] = x[:, 1] - x[:, 3] * strideY / 2  # top left y
+        y[:, 2] = x[:, 0] + x[:, 2] * strideX / 2  # bottom right x
+        y[:, 3] = x[:, 1] + x[:, 3] * strideY / 2  # bottom right y
+        return y
 
-    onnx_session["session"] = session
-    onnx_session["inputs_name"] = inputs_name
-    onnx_session["pre_execution"] = pre_execution
-    onnx_session["post_execution"] = post_execution
+    def non_max_suppression(self, input_data):
+        inputs = self.get_inputs
+        strideX = self.attr["img_width"] / 32
+        strideY = self.attr["img_height"] / 32
+        conf_thresh = self.attr["conf_thresh"]
+        iou_thresh = self.attr["iou_thresh"]
+        classes = input_data[0]
+        boxes = input_data[1]
+        boxes_size = boxes.shape[0]
+        class_num = classes.shape[1]
+        boxes = self.xywh2xyxy(boxes, strideX, strideY)
+        class_result = np.nonzero(classes > conf_thresh)
+        box_match_thresh_index = class_result[0]
+        class_index = class_result[1]
+        scores = classes[np.nonzero(classes > conf_thresh)]
 
-    return onnx_session
+        valid_boxes = boxes[box_match_thresh_index,:]
+        keep = []
+        if len(box_match_thresh_index) > 0:
+            x1 = valid_boxes[:, 0]
+            y1 = valid_boxes[:, 1]
+            x2 = valid_boxes[:, 2]
+            y2 = valid_boxes[:, 3]
+            areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+            order = scores.argsort()[::-1]
 
+            while order.size > 0:
+                i = order[0] # pick maxmum iou box
+                keep.append(i)
+                xx1 = np.maximum(x1[i], x1[order[1:]])
+                yy1 = np.maximum(y1[i], y1[order[1:]])
+                xx2 = np.minimum(x2[i], x2[order[1:]])
+                yy2 = np.minimum(y2[i], y2[order[1:]])
 
-def run_session(session_info, input_data):
-    pre_len = len(session_info["pre_execution"])
-    pre_results = []
-    post_results = []
-    net_inference_results = []
-    for i in range(pre_len):
-        pre_execution = session_info["pre_execution"][i]
-        result = pre_execution["func"](input_data)
-        pre_results.append(result)
-        input_data = result
+                w = np.maximum(0.0, xx2 - xx1 + 1) # maximum width
+                h = np.maximum(0.0, yy2 - yy1 + 1) # maxiumum height
+                inter = w * h
+                ovr = inter / (areas[i] + areas[order[1:]] - inter)
 
-    net_inference_results = onnx_session["session"].run([], input_data)
-    input_data = net_inference_results
+                inds = np.where(ovr <= iou_thresh)[0]
+                order = order[inds + 1]
+        
+        results = []
+        if keep != []:
+            results.append(valid_boxes[keep,:])
+            results.append(class_index[keep])
+        else:
+            results = [[], []]
+        return results
 
-    for i in range(pre_len):
-        post_execution = session_info["post_execution"][i]
-        result = post_execution["func"](input_data)
-        post_results.append(result)
-        input_data = result
+        
+    def execute(self, input_data):
+        result_data = []
+        assert(len(input_data) == 2)
+        result_data.extend(self.non_max_suppression(input_data))
+        output_name = self.get_outputs()
+        return dict(zip(output_name,result_data))
 
-    results = {}
-    results["pre_results"] = pre_results
-    results["net_results"] = net_inference_results
-    results["post_results"] = post_results
+class Network:
+    net_index = 0
+    def __init__(self, net_name = ""):
+        if net_name == "":
+            self.net_name = "Network_" + str(Network.net_index)
+            Network.net_index += 1
+            self.nodes = {}
+            self.nodes_names = set()
+            self.topo_order = []
+            self.blobs = {}
+
+    def insert_node(self, node):
+        self.nodes[node.name] = node
+        self.nodes_names.add(node.name)
+    
+    def get_blob_data(self, input_names):
+        result = []
+        for i in range(len(input_names)):
+            result.append(self.blobs[input_names[i]])
+        return result
+    
+    def set_blob_data(self, inputs_data):
+        for name, data in inputs_data.items():
+            self.blobs[name] = data
+
+    def generate_topo_order(self, input_names):
+        self.topo_order = []
+        temp_names = input_names
+        all_nodes = copy.deepcopy(self.nodes_names)
+        while len(all_nodes) != 0:
+            topo_flag = False
+            clone_nodes = copy.deepcopy(all_nodes)
+            for node_name in all_nodes:
+                node_info = self.nodes[node_name]
+                node_input_names = node_info.get_inputs()
+                exist_flag = True
+                for i in range(len(node_input_names)):
+                    if node_input_names[i] not in temp_names:
+                        exist_flag = False
+                        break
+                if exist_flag == True:
+                    self.topo_order.append(node_name)
+                    temp_names.extend(node_info.get_outputs())
+                    topo_flag = True
+                    clone_nodes.remove(node_name)
+            all_nodes = copy.deepcopy(clone_nodes)
+            if topo_flag == False:
+                print("topo order generate fail!\n")
+                assert topo_flag
+
+    def inference(self, inputs_data, outputs_data):
+        self.set_blob_data(inputs_data)
+        for index in range(len(self.topo_order)):
+            curr_node = self.nodes[self.topo_order[index]]
+            blob_names = curr_node.get_inputs()
+            curr_inputs = self.get_blob_data(blob_names)
+            temp_results = curr_node.execute(curr_inputs)
+            self.set_blob_data(temp_results)
+        
+        result = self.get_blob_data(outputs_data)
+        return result
+
+    def export_json(self, file_name=""):
+        json_result = {}
+        execution_node_info = {}
+        for i in range(len(self.topo_order)):
+            node = self.nodes[self.topo_order[i]]
+            execution_node_info[self.topo_order[i]] = node.get_execution_info()
+
+        if file_name == "":
+            file_name = "net_inference.json"
+        json_result["node_info"] = execution_node_info
+        json_result["topo_order"] = self.topo_order
+        json_str = json.dumps(json_result)
+        with open(file_name, 'w') as f:
+            f.write(json_str)
--- python_scripts/test_onnx_model_with_execution.py
+++ python_scripts/test_onnx_model_with_execution.py
@@ -2,24 +2,63 @@ import time
 import execution
 import cv2
 
+
 if __name__ == "__main__":
     test_img_file = "../example/yolov3/bus.jpg"
     test_input = cv2.imread(test_img_file)
-    # construct session
-    session_info = execution.construct_onnx_session("../example/yolov3/yolov3-tiny.onnx", pre_execution, post_execution)
-    input_data = {}
-    input_data[session_info.inputs_name[0]]  = test_input
-    # prepare execution
-    pre_execution = []
-    post_execution = []
-    temp_execution = execution.create_datatype_convert_execution("ConvertUint8ToFloat32")
-    pre_execution.append(temp_execution)
-    temp_execution = execution.create_normalization_execution()
-    pre_execution.append(temp_execution)
-    temp_execution = execution.create_yolo_nms_execution(512, 384)
-    post_execution.append(temp_execution)
-    # run session with results(execution result and net inference result)
-    session_results = execution.run_session(session_info, input_data)
+    # construct network
+    network = execution.Network()
+    # 1 brg to rgb
+    bgr2rgb_node = execution.DataFormatConvertExecution(["bgr_image",], ["rgb_image",])
+    attr = {}
+    attr["convert_type"] = "BGR2RGB"
+    bgr2rgb_node.init_attr(attr)
+    network.insert_node(bgr2rgb_node)
+
+    # 2 reshape rgb to 1 * h * w * c
+    inputs = bgr2rgb_node.get_outputs()
+    reshape_node = execution.ReshapeExecution(inputs, ["reshape_image",])
+    attr = {}
+    shape = [1, ]
+    img_shape = test_input.shape
+    shape.extend(list(img_shape))
+    attr["shape"] = shape
+    reshape_node.init_attr(attr)
+    network.insert_node(reshape_node)
+
+    # 3 transpose nhwc to nchw
+    inputs = reshape_node.get_outputs()
+    transpose_node = execution.TransposeExecution(inputs, ["transpose_img",])
+    attr = {}
+    attr["perm"] = [0, 3, 1, 2]
+    transpose_node.init_attr(attr)
+    network.insert_node(transpose_node)
+
+    # 4 normalization 
+    inputs = transpose_node.get_outputs()
+    normalization_node = execution.NormalizationExecution(inputs, ["images",])
+    attr = {}
+    attr["alpha"] = 0.0
+    attr["beta"] = 255.0
+    normalization_node.init_attr(attr)
+    network.insert_node(normalization_node)
+
+    # 5 onnx model 
+    inputs = normalization_node.get_outputs()
+    onnx_file = "../example/yolov3/yolov3-tiny.onnx"
+    onnx_node = execution.OnnxExecution(onnx_file, inputs, ["classes", "boxes"])
+    network.insert_node(onnx_node)
+
+    # 6 yolo nms
+    inputs = onnx_node.get_outputs()
+    nms_node = execution.YoloNMSExecution(inputs, ["nms_boxes", "nms_classes"])
+    network.insert_node(nms_node)
+
+    # 7 inference network
+    outputs = nms_node.get_outputs()
+    network.generate_topo_order(["bgr_image",])
+    result = network.inference({"bgr_image": test_input}, outputs)
     
-    print(session_results[0])
+    # 8 save network info to json file
+    network.export_json()
 

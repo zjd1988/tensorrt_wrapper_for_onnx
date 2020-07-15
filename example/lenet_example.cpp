16116a55525a9967ad402b9d4b38bf05035ab293
zjd1988 <942002795@qq.com>
2020-07-15 16:19:52 +0800

    update lenet example code

--- example/lenet_example.cpp
+++ example/lenet_example.cpp
@@ -13,17 +13,21 @@ using namespace tensorrtInference;
 #define GRAPH_JSON_FILE(net)    net "/net_graph.json"
 #define GRAPH_WEIGHTS_FILE(net) net "/net_weights.bin"
 #define GRAPH_ENGINE_FILE(net)  net "/net.engine"
+#define INFERENCE_JSON_FILE(net) net "/net_inference.json"
 #define SAVE_ENGINE 1
 #define FP16_FLAG false
 
 
+#define BACTCH_SIZE 1
+#define CHANNEL_SIZE 1
+#define HEIGHT_SIZE 32
+#define WIDTH_SIZE 32
 
-void initInputData(std::map<std::string, void*> hostMemMap)
+void initInputData(float* data)
 {
-    auto input = (float*)(hostMemMap["input"]);
-    for(int i = 0; i < 32 * 32; i++)
+    for(int i = 0; i < BACTCH_SIZE * CHANNEL_SIZE * HEIGHT_SIZE * WIDTH_SIZE; i++)
     {
-        input[i] = 1.0f;
+        data[i] = 1.0f;
     }
 }
 void printOutputData(std::map<std::string, void*> hostMemMap)
@@ -40,32 +44,32 @@ int main()
     std::string jsonFileName    = GRAPH_JSON_FILE(NET_NAME);
     std::string weightsFileName = GRAPH_WEIGHTS_FILE(NET_NAME);
     std::string engineFileName  = GRAPH_ENGINE_FILE(NET_NAME);
-    // save engine file
+    std::string inferenceFileName = INFERENCE_JSON_FILE(NET_NAME);
 #if SAVE_ENGINE
+    // save engine file
     tensorrtEngine engine(jsonFileName, weightsFileName, FP16_FLAG);
     engine.saveEnginePlanFile(engineFileName);
 #else
     //engine inference
-    // std::string bmpFile = "gray_test.bmp";
-    // cv::Mat colorBmp = cv::imread(bmpFile.c_str());
-    // cv::Mat grayBmp;
-    // cv::Mat inputBmp( 721, 1281, CV_8UC1, cv::Scalar(0));
-    // cv::Rect rect(0, 0, 1280, 720);
-    // cv::cvtColor(colorBmp, grayBmp, cv::COLOR_BGR2GRAY);
-    // grayBmp.copyTo(inputBmp(rect));
-    // cv::Mat inputFloatBmp( 721, 1281, CV_32FC1, cv::Scalar(0));
-    // inputBmp.convertTo(inputFloatBmp, CV_32F);
-    tensorrtEngine engine(engineFileName);
-    auto hostMem = engine.getBindingNamesHostMemMap();
-    initInputData(hostMem);
+    float* data = (float*)malloc(BACTCH_SIZE * CHANNEL_SIZE * HEIGHT_SIZE * WIDTH_SIZE * sizeof(float));
+    initInputData(data);
+    tensorrtEngine engine(inferenceFileName);
+    std::cout << "run here!" << std::endl;
+    // auto hostMemIndex = engine.getBindingNamesIndexMap();
     
-    for (int i = 0; i < 100; i++) {
-        auto start = std::chrono::system_clock::now();
-        engine.doInference(true);
-        auto end = std::chrono::system_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-    }
-    printOutputData(hostMem);
+    std::map<std::string, void*> inputs;
+    inputs["input"] = (void*)data;
+    engine.prepareData(inputs);
+    engine.doInference(true);
+    // for (int i = 0; i < 10; i++) {
+    //     auto start = std::chrono::system_clock::now();
+    //     engine.doInference(true);
+    //     auto end = std::chrono::system_clock::now();
+    //     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
+    // }
+    auto result = engine.getInferenceResult();
+    printOutputData(result);
+    free(data);
 #endif
 
 }
